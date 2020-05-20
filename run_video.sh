422fd6f5742a6c72221d217799e2ce53270adac5
kun <quinnhugh@hotmail.com>
2020-05-20 23:19:42 +0800

    Update run_video.sh

--- run_video.sh
+++ run_video.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 xhost +localhost
-docker build . -t magicstick &&\
+docker build . -t magic_stick &&\
 # docker run --gpus all --rm --name openpose -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v `pwd`:/data magicstick /openpose/magic_stick/build/magicstick.bin --video /data/$1
 
-docker run --gpus all --rm --name openpose -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v `pwd`:/data --device /dev/video0 magicstick /openpose/magic_stick/build/magicstick.bin --camera_resolution 480x320 --render_pose 0 --camera 0
+docker run --gpus all --rm --name openpose -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v `pwd`:/data --device /dev/video0 magic_stick /openpose/magic_stick/build/magicstick.bin --camera_resolution 480x320 --render_pose 0 --camera 0
